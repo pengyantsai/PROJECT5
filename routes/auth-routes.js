fdad69f6b821702286e8bb340521365652f57ffa
pengpeng <s123171293@gmail.com>
2022-05-04 11:55:55 +0800

    finish googlelogin

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -66,7 +66,7 @@ router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   if (req.session.returnTo) {
     let newPath = req.session.returnTo;
     req.session.returnTo = "";
-    res.redirect(newPath);
+    res.redirect(newPath); // /profile
   } else {
     res.redirect("/profile");
   }
